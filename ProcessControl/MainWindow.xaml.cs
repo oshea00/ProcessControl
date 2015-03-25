9bc68a49704b38e8a24f25c3e59e747f8a3a02f6
Mike O'Shea <oshea00@gmail.com>
2015-03-25 13:16:02 -0700

    first commit

---
+++ ProcessControl/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProcessControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        RemoteControl _remote;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _remote = new RemoteControl(this);
+            _remote.Start();
+        }
+
+        public void btnAddText_Click(object sender, RoutedEventArgs e)
+        {
+            tbDoc.Text += txtText.Text;
+        }
+
+        public void btnReplace_Click(object sender, RoutedEventArgs e)
+        {
+            tbDoc.Text = txtText.Text;
+        }
+
+        public void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            tbDoc.Text = string.Empty;
+        }
+    }
+}
---
+++ ProcessControl/RemoteControl.cs
@@ -0,0 +1,72 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessControl
+{
+    class RemoteControl
+    {
+        Task _queueListener = null;
+        MainWindow _main = null;
+
+        public RemoteControl(MainWindow main)
+        {
+            _main = main;
+        }
+
+        public void Start()
+        {
+            _queueListener = Task.Factory.StartNew(() => {
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.QueueDeclare("RemoteControl", false, false, false, null);
+                        var consumer = new QueueingBasicConsumer(channel);
+                        channel.BasicConsume("RemoteControl", true, consumer);
+                        while (true)
+                        {
+                            var eventArgs = (BasicDeliverEventArgs) consumer.Queue.Dequeue();
+                            var body = eventArgs.Body;
+                            var message = Encoding.UTF8.GetString(body);
+                            switch (message)
+                            {
+                                case "Add": _main.Dispatcher.Invoke(() => { _main.btnAddText_Click(null, null); }); break;
+                                case "Replace": _main.Dispatcher.Invoke(() => { _main.btnReplace_Click(null, null); }); break;
+                                case "Clear": _main.Dispatcher.Invoke(() => { _main.btnClear_Click(null, null); }); break;
+                                default: break;
+                            }
+                        }
+                    }
+                }
+
+            });
+
+        }
+
+        public void Stop()
+        {
+
+        }
+
+        public void AddText(string text)
+        {
+
+        }
+
+        public void ReplaceText(string text)
+        {
+
+        }
+
+        public void ClearText()
+        {
+
+        }
+    }
+}
